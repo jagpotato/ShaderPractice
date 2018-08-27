82a6237257ad0d309f56489994e5e77563e773de
daiki <dik.piyo.piyo@gmail.com>
2018-08-27 11:52:02 +0900

    copy texture

---
+++ Assets/Scripts/Scene6/CopyImage.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyImage : MonoBehaviour {
+	[SerializeField]
+	private ComputeShader shader;
+	[SerializeField]
+	private GameObject image, imageCopy;
+
+	RenderTexture textureCopy;
+
+	void Start () {
+		int row = 128;
+		int imageSize = row * row;
+		ComputeBuffer buffer = new ComputeBuffer(imageSize, sizeof(float));
+		int kernel = shader.FindKernel("CSMain");
+		shader.SetBuffer(kernel, "buffer", buffer);
+		Texture texture = image.GetComponent<Renderer>().material.mainTexture;
+		shader.SetTexture(kernel, "tex", texture);
+		textureCopy = new RenderTexture(row, row, 0);
+		textureCopy.enableRandomWrite = true;
+		textureCopy.Create();
+		shader.SetTexture(kernel, "texCopy", textureCopy);
+
+		shader.Dispatch(kernel, row/8, row/8, 1);
+
+		// float[] data = new float[imageSize];
+		// buffer.GetData(data);
+
+		// foreach(float e in data) {
+		// 	Debug.Log(e);
+		// }
+
+		Debug.Log(textureCopy);
+		imageCopy.GetComponent<Renderer>().material.mainTexture = textureCopy;
+
+		buffer.Release();
+	}
+	
+	// void OnGUI () {
+	// 	int w = Screen.width;
+	// 	int h = Screen.height;
+	// 	int s = 512;
+	// 	GUI.DrawTexture(new Rect(w - s / 2, h - s / 2, s, s), textureCopy);
+	// }
+
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/Scene6/SetCube.cs
+++ Assets/Scripts/Scene6/SetCube.cs
@@ -3,8 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SetCube : MonoBehaviour {
+	[SerializeField]
+	private int cubeNum;
+	[SerializeField]
+	private Mesh cube;
+	[SerializeField]
+	private ComputeShader setCubeShader;
+
+  private ComputeBuffer setCubeBuffer;
+	private ComputeBuffer argsBuffer;
+	private uint[] args = new uint[] {0, 0, 0, 0, 0};
 	void Start () {
-		
+		argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
 	}
 	
 	void Update () {
